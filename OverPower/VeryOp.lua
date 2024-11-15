974b2d228c26d161925834b5018fff14c88bf95b
Baodzvip <116901121+Baodzvip@users.noreply.github.com>
2024-11-15 18:12:40 +0700

    Update VeryOp.lua

--- OverPower/VeryOp.lua
+++ OverPower/VeryOp.lua
@@ -1,4 +1,51 @@
-repeat task.wait(1) until game and game.Players.LocalPlayer and game:IsLoaded()
-getgenv().Key = "4e5c3a500a21521a0cf5b4b7"
-_G.UI_Settings = {LoadUI = false, ChooseEnabled = true, Choose = "CDKTalon", Default = {"Gun_ElectricClaw", "CDKTalon", "ECVenom"}}
-loadstring(game:HttpGet('https://raw.githubusercontent.com/RedGamer12/TNNP-SYSTEM/refs/heads/main/client/BloxFruit/BountyLoader-obfuscated.lua'))()
+repeat wait()
+until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().Key = "4e5c3a500a21521a0cf5b4b7"    
+getgenv().Setting = {
+        ["Team"] = "Pirates",
+        ["Method Click"] = {["Click Gun"] = false ,["Click Melee"] = true,["Click Sword"] = false, ["LowHealth"] = 5000, ["Delay Click"] = 0.3},
+        ["Race V4"] = {["Enable"] = true},
+        ["Webhook"] = {["Enabled"] = false,["Url Webhook"] = ""},
+        ["Misc"] = {["AutoBuyRandomandStoreFruit"] = true,["AutoBuySurprise"] = true},
+        ["SafeZone"] = {["Enable"] = true,["LowHealth"] = 4500,["MaxHealth"] = 8000,["Teleport Y"] = 2000},
+        ["Method Use Skill"] = {["Use Random"] = false,["Use Number"] = true},
+        ["Use random skill if player target low health"] = { --- suport only method use skill Number
+          ["Enabled"] = true,
+          ["Low Health"] = 4000,
+        },
+        ["Weapons"] = {
+            ["Melee"] = {
+                ["Enable"] = true,
+                ["Skills"] = {
+                    ["Z"] = {["Enable"] = true,["HoldTime"] = 0.1,["Number"] = 2},
+                    ["X"] = {["Enable"] = true,["HoldTime"] = 0.1,["Number"] = 3},
+                    ["C"] = {["Enable"] = true,["HoldTime"] = 0.1,["Number"] = 5},
+                },
+            },
+            ["Blox Fruit"] = {
+                ["Enable"] = true,
+                ["Skills"] = {
+                    ["Z"] = {["Enable"] = true,["HoldTime"] = 0.1,["Number"] = 4},
+                    ["X"] = {["Enable"] = true,["HoldTime"] = 0.1,["Number"] = 6},
+                    ["C"] = {["Enable"] = true,["HoldTime"] = 0.1,["Number"] = 9},
+                    ["V"] = {["Enable"] = false,["HoldTime"] = 0.1,["Number"] = 0},
+                    ["F"] = {["Enable"] = true,["HoldTime"] = 0.1,["Number"] = 8},
+                },
+            },
+            ["Gun"] = {
+                ["Enable"] = true,
+                ["Skills"] = {
+                    ["Z"] = {["Enable"] = true,["HoldTime"] = 0.1,["Number"] = 1},
+                    ["X"] = {["Enable"] = true,["HoldTime"] = 0.1,["Number"] = 7},
+                },
+            },
+            ["Sword"] = {
+                ["Enable"] = false,
+                ["Skills"] = {
+                    ["Z"] = {["Enable"] = true,["HoldTime"] = 1.5,["Number"] = 0},
+                    ["X"] = {["Enable"] = true,["HoldTime"] = 0.5,["Number"] = 0},
+                },
+            },
+        }
+    }
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaCat-BountyEz.lua"))()
