3ce0377e09d6d8dfdc23943e323df94ab17527fc
Baodzvip <116901121+Baodzvip@users.noreply.github.com>
2024-11-15 21:42:54 +0700

    Update VeryOp.lua

--- OverPower/VeryOp.lua
+++ OverPower/VeryOp.lua
@@ -1,4 +1,4 @@
-repeat task.wait(1) until game and game.Players.LocalPlayer and game:IsLoaded()
-getgenv().Key = "4e5c3a500a21521a0cf5b4b7"
-_G.UI_Settings = {LoadUI = false, ChooseEnabled = false, Choose = "CDKTalon", Default = {"Gun_ElectricClaw", "CDKTalon", "ECVenom"}}
-loadstring(game:HttpGet('https://raw.githubusercontent.com/RedGamer12/TNNP-SYSTEM/refs/heads/main/client/BloxFruit/BountyLoader-obfuscated.lua'))()
+getgenv().Key = "ARCHUB-WSKS-7NU7-CXPZ-DKGLXZH"
+getgenv().RaidUI = false -- For Hidden Raid Mode true = show and false = hidden
+getgenv().CustomDistance = 8 -- For Bug Can't Swipe Distance
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ChopLoris/ArcHub/main/main.lua"))()
