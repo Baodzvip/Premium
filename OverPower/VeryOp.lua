0d74af489051b23b7d65f0a1201503b8a820c9c6
Baodzvip <116901121+Baodzvip@users.noreply.github.com>
2024-11-15 13:14:38 +0700

    Update VeryOp.lua

--- OverPower/VeryOp.lua
+++ OverPower/VeryOp.lua
@@ -1,33 +1,4 @@
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+repeat task.wait(1) until game and game.Players.LocalPlayer and game:IsLoaded()
 getgenv().Key = "4e5c3a500a21521a0cf5b4b7"
-getgenv().SettingFarm = {
-    ["Roll Unit"] = {
-        ["Enabled"] = true,
-        ["Gems"] = 2000,
-        ["Name Units"] = {
-            "Itaha",
-        },
-        ["Auto Sell"] = {
-            ["Rare"] = true,
-            ["Epic"] = true,
-            ["Legendary"] = false,
-        },
-    },
-    ["Sell In Wave"] = {
-        ["Enabled"] = false,
-        ["Waves"] = 10,
-    },
-    ["Method Farm"] = {
-        ["Spam Only Act 1"] = false ,
-        ["if have unit roll will auto Next Act"] = true, -- next until unlock infinite
-    },
-    ["Black Screen"] = true,
-    ["Webhook"] = {
-        ["Enabled"] = false,
-        ["Url"] = "",
-        ["Webhook Stage Finished/Infinite"] = true,
-    },
-
-}
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/BananaCat-KaitunAR.lua"))()
+_G.UI_Settings = {LoadUI = false, ChooseEnabled = true, Choose = "CDKTalon", Default = {"Gun_ElectricClaw", "CDKTalon", "ECVenom"}}
+loadstring(game:HttpGet('https://raw.githubusercontent.com/RedGamer12/TNNP-SYSTEM/refs/heads/main/client/BloxFruit/BountyLoader-obfuscated.lua'))()
