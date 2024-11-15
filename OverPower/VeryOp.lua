057b54a51d232a1eba97f15455dde9935dbcc3f7
Baodzvip <116901121+Baodzvip@users.noreply.github.com>
2024-11-15 21:13:20 +0700

    Update VeryOp.lua

--- OverPower/VeryOp.lua
+++ OverPower/VeryOp.lua
@@ -1,4 +1,4 @@
 repeat task.wait(1) until game and game.Players.LocalPlayer and game:IsLoaded()
 getgenv().Key = "4e5c3a500a21521a0cf5b4b7"
-_G.UI_Settings = {LoadUI = true, ChooseEnabled = true, Choose = "CDKTalon", Default = {"Gun_ElectricClaw", "CDKTalon", "ECVenom"}}
+_G.UI_Settings = {LoadUI = true, ChooseEnabled = false, Choose = "CDKTalon", Default = {"Gun_ElectricClaw", "CDKTalon", "ECVenom"}}
 loadstring(game:HttpGet('https://raw.githubusercontent.com/RedGamer12/TNNP-SYSTEM/refs/heads/main/client/BloxFruit/BountyLoader-obfuscated.lua'))()
